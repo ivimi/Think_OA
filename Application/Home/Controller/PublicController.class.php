321597dbca6d2d6ab7797f6213f785194e861d13
ivim <admin@ivim.vip>
2017-04-09 14:25:40 +0800

    Login

--- Application/Home/Controller/PublicController.class.php
+++ Application/Home/Controller/PublicController.class.php
@@ -9,19 +9,22 @@ class PublicController extends Controller{
     // 用户登录
     public function login(){
         // TODO:测试数据
-        $userName = 'admin';
-        $passWord = 'admin';
-        // TODO:检测用户名类型,邮箱或手机或用户名
-        $type = chk_username_type($userName);
-        // TODO:检测验证码
+        if(I('post.')){
+            $userName = I('post.userName');
+            $passWord = I('post.passWord');
+            // TODO:检测用户名类型,邮箱或手机或用户名
+            $type = chk_username_type($userName);
+            // TODO:检测验证码
 
-        // 调用 User API 接口登录
-        $userApi = new UserApi();
-        $userApiLoginResult = $userApi->login($userName,$passWord,$type);
-        if($userApiLoginResult){
-            $this->success('登录成功!',U('Index/index'));
-        }else{
-            $this->error('登录失败!',U('Index/login'));
+            // 调用 User API 接口登录
+            $userApi = new UserApi();
+            $userApiLoginResult = $userApi->login($userName,$passWord,$type);
+            if($userApiLoginResult){
+                $this->ajaxReturn('登录成功!');
+            }else{
+                $this->ajaxReturn('登录失败!');
+            }
         }
+
     }
 }
